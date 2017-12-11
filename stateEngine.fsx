9940c74a62416befaa6305cc3bea4e4ff9b6c584
Zattri <daninjakidy@gmail.com>
2017-12-11 18:08:02 +0000

    Added in updating location objects set

--- stateEngine.fsx
+++ stateEngine.fsx
@@ -1,3 +1,4 @@
+// TYPE DEFINITIONS
 open System
 
 type Command =
@@ -46,40 +47,39 @@ type InteractionTuple = Command * int * int
 type ResultTuple = string * int * string
 
 
-let checkwObjectState (wObject:WorldObject) checkState = 
-  checkState = wObject.stateNum
+// =====================================================================================
+// DEFAULT VALUES FOR INTERACTIONS, ITEMS, OBJECTS AND LOCATIONS
 
-let incrementwObjectState (wObject:WorldObject) = 
-  {wObject with stateNum = wObject.stateNum + 1}
-
-
-
-// Testing
-let testLoc = {
-    name = "Pyramid";
-    state = 0;
-    items = Set.ofList [
-      {id=0; name="Dagger"};
-      {id=1; name="Sword"}
-    ];
-    objects = Set.ofList [
-      {id=1; name = "Pillar"; stateNum = 0; stateString="The pillar has an eagle carved into it"}; 
-      {id=2; name="Door"; stateNum=1; stateString="The door has a glyph of a sun on it"}
-    ]
-}
-
-// World Object definitions
-let obj = {id=1; name="Button"; stateNum=0; stateString="The button is untouched"}
-let obj2 = {id=2; name="Lever"; stateNum=0; stateString="The lever is upright"}
-
-
-// Interaction Dictionary Definition 
-// <Key> : <Item>
 // (Command, Object ID, Specific State Number) : (Interaction Text, New State Num, New State String)
 let interactionDict = dict[
     (Push, 1, 0), ("You have pushed the button", 4, "The button on the wall is pushed in");
     (Pull, 2, 0), ("You have pulled the lever", 4, "The lever on the wall is pulled down");
 ]
+
+let obj1 = {id=1; name="Button"; stateNum=0; stateString="The button is untouched"}
+let obj2 = {id=2; name="Lever"; stateNum=0; stateString="The lever is upright"}
+
+let item1 = {id=0; name="Dagger"}
+let item2 = {id=1; name="Sword"}
+
+let testLoc = {
+    name = "Pyramid";
+    state = 0;
+    items = Set.ofList [item1; item2];
+    objects = Set.ofList [obj1; obj2]
+}
+
+// =============================================================================================
+
+
+// Error checking for function done if error tuple is returned or not from checkInteractionKey
+let updatewObjectStates (wObject:WorldObject) (newStateNum:int) (newStateString:string) = 
+  {wObject with stateNum = newStateNum; stateString = newStateString}
+
+let updateLocationObjectsSet (wObject:WorldObject) (locationRecord:Location) (resultTuple:ResultTuple) = 
+  let _,newStateNum,newStateString = resultTuple
+  let newObject = updatewObjectStates wObject newStateNum newStateString
+  {locationRecord with objects = locationRecord.objects.Remove(wObject).Add(newObject)}
 
 
 // This function is really trash - Pls refine it, maybe 2 functions nested?
@@ -92,11 +92,41 @@ let formatInteractionAndErrorTuples (inputTuple:InputTuple) =
 // Checks the interaction array to see if the command can be applie to the given object
 let checkInteractionKey (inputTuple:InputTuple) =
   let tuples = formatInteractionAndErrorTuples inputTuple
-  [if interactionDict.ContainsKey (fst tuples) then
+  // Need options on returning things
+  [if (interactionDict.ContainsKey (fst tuples)) then
     yield interactionDict.Item(fst tuples) 
   else 
-    yield (snd tuples)]
+    yield (snd tuples)].Head
+// Returns a list pls fix
 
-checkInteractionKey (Push, obj)
+
+// Also needs player location - testLoc needs to be where the object is located / where the player is currently (should be the same)
+let processCommand (inputTuple:InputTuple) = 
+let command,wObject = inputTuple
+updateLocationObjectsSet wObject testLoc (checkInteractionKey (command, wObject))
+
+
+// Testing Area
+printfn "%A" testLoc // Location before
+processCommand (Push, obj1) // Location after
+
+updateLocationObjectsSet obj1 testLoc (checkInteractionKey (Push, obj1))
+
 checkInteractionKey (Pull, obj2)
 checkInteractionKey (Push, obj2)
+
+updatewObjectStates obj1 0 "The lever is upright"
+updatewObjectStates obj1 1 "The lever is pulled"
+
+(*
+TODO - 
+Need to add options to checkInteractionkey
+Based on options update object or not
+Figure out how we want to update items
+- Update the sets in Locaton based off of this
+  - Use the Set functions to update them and make new records
+- Update Location state based off of whether all objects in the set are complete or not
+
+GG EZ
+We Gucci
+*)
