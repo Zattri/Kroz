74f78312c94c2364ef3697a728e1a7b9f173bede
reece8765 <reece8765@icloud.com>
2017-12-14 15:23:31 +0000

    Added Comments, replaced horriffic code and created problems
    Used code from main to attempt validation on user inputs. This doesn't work correctly as the code is still wrong but on the right track.
    Added comments to code
    Introduced a few lovely errors to fix later on

--- Program.fs
+++ Program.fs
@@ -1,62 +1,88 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
-#indent "off"
+﻿open System.Diagnostics
+open System.Runtime.Remoting.Messaging
 
 //Some of these types are EXAMPLES and are to be replaced later on. These are clearly marked.
 
-type Item = 
+type Item =  //EXAMPLE
   {
     id:int;
     name:string; 
   }
+  
 
-type WorldObject = 
-  {
-    id:int;
-    name:string;
-    stateNum:int;
-    stateString:string;
-  }
+type WorldObject =  //Not my code
+    {
+     id:int;
+     name:string;
+     stateNum:int;
+     stateString:string;
+    }
 
-type Location = 
-  {
-    name:string;
-    state:int;  
-    items: Set<Item>;
-    objects: Set<WorldObject>;
-  }
-
-
-type Helpmsg = 
-    { helpText: string }
+type Location = //Not my code
+    {
+     name:string;
+     state:int;  
+     items: Set<Item>;
+     objects: Set<WorldObject>;
+    }
 
 
-type Hints = 
+type Hints =  //Hints are based on location and an integer, if the integer is 0, no hints are given.
     { Room: Location;
       Counter: int;
       Message: string }
 
-type Action = 
-    { Name:string }
+type command = //All Commands are here
+    | Use
+    | Move
+    | Help of string list
+    | Hint
+    | Rotate
+    | Go
+    | Search
+    | Look
+    | Open
+    | Close
+    | Push
+    | Pull
+    | NotValid of string list
 
-type command = //All commands in this type will be referenced to other types
-    | Use of Action //When you perform an action command, the program should look for the action type.
-    | Move of Action
-    | Help of Helpmsg //Help will look fo the type Helpmsg
-    | Hint of Hints //Hints will look for type Hints
-    | Rotate of Action
-    | Go of Action
-    | Search of Location //Search will give details of objects in the room
-    | Look of Location //Look will give details of everything in the room, including items and objects without specific detail
-    | Open of Action
-    | Close of Action
-    | Push of Action
-    | Pull of Action
-    | NotValid of Helpmsg option //This will be looking for details with optional inputs. If nothing is typed, don't do anything and if something is typed but not in the list, return a help message
+type object = //EXAMPLE
+    |Spoon
+    |Log
+    |Nothing
 
+        
+let validateCommand(command:string, object:string) = 
+    if command = "Use" then Use
+    elif command = "Move" then Move
+    elif command = "Help" then Help ["The sytax for this game is: command object. For example: Push Pillar. You can also use object ID's if you know them"]
+    elif command = "Hint" then Hint
+    elif command = "Rotate" then Rotate
+    elif command = "Go" then Go
+    elif command = "Search" then Search
+    elif command = "Look" then Look
+    elif command = "Open" then Open
+    elif command = "Close" then Close
+    elif command = "Push" then Push
+    elif command = "Pull" then Pull
+    else NotValid ["Please enter a valid command!"]
 
-    let cmd = System.Console.ReadLine();
-    if cmd == "" then
-        command.Use
-        else
-            printfn "ERROR: Not a valid command or item! Type 'help' for info"
+type result = 
+    |Valid
+    |Invalid of string list
+
+let (<&>) inp1 inp2 x = //Perhaps this is wrong? Need to work on this some more as only examples are used.
+  match inp1 x, inp2 x with
+  | Use, Spoon -> Valid
+  | Use, Log -> Invalid ["Can't use this command on this object!"]
+  | Help[""], _ -> Valid
+  |_, _ -> Invalid ["No command specified."]
+
+let command = System.Console.ReadLine() //Takes in input for what command the user wants
+let objoritm = System.Console.ReadLine() //Takes in input for item or object the user wants to use a command on
+
+let validate = 
+    validateCommand <&> //States this is incorrect
+
+validate(command, objoritm) //Supposed to validate the inputs to make sure the user can perform a comman on and object or item
