4f6fda38d7c63f5bc3adf1f90dac345b6d654cef
Zattri <daninjakidy@gmail.com>
2018-01-04 13:34:16 +0000

    Converting to FSharp program

--- stateEngine.fs
+++ stateEngine.fs
@@ -37,8 +37,8 @@ type Location =
     objects: Set<WorldObject>;
   }
 
-// Input Command * WorldObject
-type InputTuple = Command * WorldObject
+// Input Command * string (or World Object name)
+type InputTuple = Command * string
 
 // Input Command * Object ID * Specific Object State For Interaction
 type InteractionTuple = Command * int * int
@@ -55,26 +55,20 @@ type ResultTuple = string * List<ObjectUpdateTuple>
 
 // (Command, Object ID, Specific State Number) : (Interaction Text, New State Num, New State String)
 let interactionDict = dict[
-    (Push, 1, 0), ("You have pushed the button", [(1, 4, "The button is pushed in"); (2, 4, "The lever is pulled")]);
-    (Pull, 2, 0), ("You have pulled the lever", [(2, 4, "The lever is pulled"); (1, 4, "The button is pushed in")]);
+  (Push, 1, 0), ("You push the button", [(1, 4, "The button is pushed in"); (2, 1, "The lever is upright")]);
+  (Pull, 2, 0), ("The lever will not move", [(2, 0, "The lever is upright")]);
+  (Pull, 2, 1), ("The lever clunks down into place and there is a 'chink' sound from the door", [(2, 4, "The lever is pulled down"); (3, 1, "The door is unlocked")])
+  (Open, 3, 0), ("The door does not move", [(3, 0, "The door is closed, it seems to be locked")]);
+  (Open, 3, 1), ("The door swings open", [(3, 4, "The door is open")]);
 ]
-
-// let newInteractionDict = dict[
-//   (command, objId, stateOfObject), (resultTuple, [(ObjectUpdateTuple); (ObjectUpdateTuple)])
-// ]
 
 let obj1 = {id=1; name="button"; stateNum=0; stateString="The button is untouched"}
 let obj2 = {id=2; name="lever"; stateNum=0; stateString="The lever is upright"}
+let obj3 = {id=3; name="door"; stateNum=0; stateString="The door is closed"}
 
 let item1 = {id=0; name="dagger"}
 let item2 = {id=1; name="sword"}
 
-let testLoc = {
-    name = "pyramid";
-    state = 0;
-    items = Set.ofList [item1; item2];
-    objects = Set.ofList [obj1; obj2]
-}
 
 // =============================================================================================
 // Sands of Time test room 
@@ -82,14 +76,14 @@ let sandsOfTime = {
   name = "Sand Room";
   state = 0;
   items = Set.ofList [];
-  objects = Set.ofList [obj1; obj2]
+  objects = Set.ofList [obj1; obj2; obj3]
 }
 
 
 // =============================================================================================
 
 // Temp Variable - Create some sort of interface that alters the current location of the player
-let currentLocation = sandsOfTime
+let mutable currentLocation = sandsOfTime
 // =============================================================================================
 
 (*
@@ -102,19 +96,23 @@ let currentLocation = sandsOfTime
 let updateObjectStates (newObjStatsList:List<ObjectUpdateTuple>) =  
   let sortedObjStatsList = newObjStatsList |> List.sortBy (fun (id,_,_) -> id)
   let oldObjects = Set.filter (fun elem -> List.exists (fun (id,_,_) -> id = elem.id) sortedObjStatsList) currentLocation.objects
-  [ oldObjects; 
-    Seq.zip oldObjects sortedObjStatsList |> 
+  let returnedList = [ oldObjects; 
+    Seq.zip oldObjects sortedObjStatsList |>
       Seq.map (fun item -> 
         let object, (_, newStateNum, newStateString) = item
         {object with stateNum = newStateNum; stateString = newStateString}
       ) |> Set.ofSeq
   ]
+  //printfn "RETURNED LIST %A" returnedList // Remove this and returnedList variable after debugging
+  returnedList
 
 
 // Update the objects set at the current location, removing the old objects and adding the updated ones
 let updateLocationObjectsSet (updateList:List<ObjectUpdateTuple>) = 
   let objectsList = updateObjectStates updateList
-  {currentLocation with objects = currentLocation.objects |> Set.difference(objectsList.Head) |> Set.union(objectsList.Tail.Head)}
+  let returnedLocation = {currentLocation with objects = Set.difference currentLocation.objects objectsList.Head |> Set.union(objectsList.Tail.Head)}
+  //printfn "RETURNED LOCATION %A" returnedLocation.objects // Remove this and returnedLocation after debugging
+  returnedLocation
 
 (*
 Checks the interaction array to see if the command can be applied to the given object
@@ -123,7 +121,7 @@ Checks the interaction array to see if the command can be applied to the given o
 *) 
 let checkInteractionKey (command:Command, wObject:WorldObject) =
   try 
-    let outputText,matchedItem = interactionDict.Item(command, wObject.id, wObject.stateNum)
+    let (outputText,matchedItem) = interactionDict.Item((command, wObject.id, wObject.stateNum))
     printfn "%A" outputText
     Some matchedItem
   with _ ->  
@@ -147,18 +145,28 @@ let processCommand (command:Command, objectName:string) =
     printfn "There is no %s in your current location" objectName
     currentLocation
     
-  
-  
 
+// A function made specifically for 
 
 // Testing Area - Careful, messy
+let testHarness = 
+  printfn "%A" currentLocation
+  currentLocation <- processCommand (Push, "button")
+  printfn "%A" currentLocation
+  currentLocation <- processCommand (Pull, "lever")
+  printfn "%A" currentLocation
+  currentLocation <- processCommand (Open, "door")
+  printfn "%A" currentLocation
 
-currentLocation
-processCommand (Pull, "lever")
+
+[<EntryPoint>]
+let main argv = 
+    testHarness    
+    Console.ReadLine() |> ignore
+    0
 
 (*
 UPDATED TO DO - 
-Convert to an actual program and not just a script
 Figure out how we want to update items
 
 FOR THE OTHERS TO WORK ON - 
